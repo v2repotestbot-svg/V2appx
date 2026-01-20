bcf16792609a73c92b3a702af778ae00d70d7637
v2repotestbot-svg <v2repotestbot@gmail.com>
2026-01-20 06:07:14 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 import subprocess, os
 from config import HEADERS
 
+FFMPEG_PATH = "./ffmpeg" if os.path.exists("./ffmpeg") else "ffmpeg"
+
 def run_ffmpeg(m3u8, out_file):
     os.makedirs(os.path.dirname(out_file), exist_ok=True)
 
     cmd = [
-        "ffmpeg", "-y",
+        FFMPEG_PATH, "-y",
         "-loglevel", "error",
         "-stats",
         "-headers", HEADERS,
@@ -17,4 +19,3 @@ def run_ffmpeg(m3u8, out_file):
 
     p = subprocess.run(cmd)
     return p.returncode == 0 and os.path.exists(out_file)
-  
