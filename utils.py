de7b2fb9d47391a9526e6fa5591bfc8956b2e9dc
v2repotestbot-svg <v2repotestbot@gmail.com>
2026-01-20 05:43:08 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,20 @@
+import subprocess, os
+from config import HEADERS
+
+def run_ffmpeg(m3u8, out_file):
+    os.makedirs(os.path.dirname(out_file), exist_ok=True)
+
+    cmd = [
+        "ffmpeg", "-y",
+        "-loglevel", "error",
+        "-stats",
+        "-headers", HEADERS,
+        "-i", m3u8,
+        "-c", "copy",
+        "-bsf:a", "aac_adtstoasc",
+        out_file
+    ]
+
+    p = subprocess.run(cmd)
+    return p.returncode == 0 and os.path.exists(out_file)
+  
